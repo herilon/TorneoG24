ee330d0e6c869be716023f054ea6c60a87a3ef9d
Heri <hlondono@misena.edu.co>
2022-09-10 08:50:21 -0500

    Validaciones

--- Torneo.App.Dominio/Entidades/DirectorTecnico.cs
+++ Torneo.App.Dominio/Entidades/DirectorTecnico.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Torneo.App.Dominio
 {
     public class DirectorTecnico
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre del D.T")]
+        [Required(ErrorMessage = "El nombre del DT es obligatorio")]
         public string Nombre { get; set; }
+        [Display(Name = "Numero de documento")]
+        [Required(ErrorMessage = "El documento del DT es obligatorio")]
         public string Documento { get; set; }
+        [Required(ErrorMessage = "El telefono del DT es obligatorio")]
         public string Telefono { get; set; }
     }
 }
--- Torneo.App.Dominio/Entidades/Municipio.cs
+++ Torneo.App.Dominio/Entidades/Municipio.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace Torneo.App.Dominio
 {
     public class Municipio
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre del municipio")]
+        [Required(ErrorMessage = "El nombre del municipio es obligatorio")]
         public string Nombre { get; set; }
     }
 }
