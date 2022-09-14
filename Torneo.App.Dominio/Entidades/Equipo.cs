32421ea174110467aeb2c1458ea4c0e434bd7d4b
Heri <hlondono@misena.edu.co>
2022-09-13 19:15:56 -0500

    Validacion Equipo

--- Torneo.App.Dominio/Entidades/Equipo.cs
+++ Torneo.App.Dominio/Entidades/Equipo.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 namespace Torneo.App.Dominio
 {
     public class Equipo
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
         public Municipio Municipio { get; set; }
         public DirectorTecnico DirectorTecnico { get; set; }
